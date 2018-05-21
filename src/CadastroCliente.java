f0d804ea98de6d96baf27d8b87d3f6cff91718c5
Marcop <marciopedro15@gmail.com>
2018-05-21 20:32:50 -0300

    Inicializando Repositorio

---
+++ src/CadastroCliente.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alunos
+ */
+public class CadastroCliente {
+    int atual =0;
+    
+    String nomes [] = new String [100];
+    String dataNascimentos[] = new  String [100];
+    int cpfs [] = new int[100];
+    String telephones []=new String[100]; //telefone
+    String mobilephones [] = new String [100]; //celular
+    String ufs [] = new  String [100]; //bairro
+    String cities[] = new String [100]; //Cidades
+    String neighborhoods [] = new String[100];//Bairros
+    String places [] = new String [100]; // lougradouros
+    String numbers [] = new String[100]; //numeros
+    String addOns  [] = new String [100];// complementos
+    
+    
+}
