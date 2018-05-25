0748ddb16092f0c549d11a86308813243999be72
BorbaRi <ricardo_borba06@hotmail.com>
2018-05-25 08:54:34 -0300

    Desenvolvimento

--- src/Vendas.java
+++ src/Vendas.java
@@ -1,42 +1,47 @@
-
 import javax.swing.JOptionPane;
 
 /**
  *
- * @author Alunos
+ * @author Rciardo Borba & Marcio Pedro Schiehl
  */
 public class Vendas {
+    
+    
 
-    int maiorVendas = Integer.MIN_VALUE;
-    int maiorCliente = Integer.MIN_VALUE;
-    String[] marcas = new String [100];
-    String nomesFuncionario[] = new String[100];
-    String nomesCliente[] = new String[100];
-    String marcaMaiorPreco[] = new String[100];
+    int[] numVendas = new int[100]; 
+    String[] nomeCelular = new String [100];
+    String[] nomeFuncionario= new String[100];
+    String[] nomeCliente= new String[100];
+    double[] valorVendas = new double[100];
     int atual = 0;
 
     public void solicitarInformacao(int posicao) {
-        nomesFuncionario[posicao] = JOptionPane.showInputDialog("Digite a Funcionario");
-        nomesCliente[posicao] = JOptionPane.showInputDialog("Digite a Cliente");
-        marcas[posicao] = JOptionPane.showInputDialog("Digite produto vendido");
+        nomeFuncionario[posicao] = JOptionPane.showInputDialog("Digite a Funcionario");
+        numVendas[posicao] = Integer.parseInt(JOptionPane.showInputDialog("Digite quantidade vendida"));
+        nomeCliente[posicao] = JOptionPane.showInputDialog("Digite 0 Cliente");
+        nomeCelular[posicao] = JOptionPane.showInputDialog("Digite produto vendido");
     }
-
-    for(int i=0;i<atual;i++){
-        if(nomesFuncionario[i] > maiorVendas[i]){
-                maiorVendas[i] = nomesFuncionario[i];
-        }
-        if(nomesCliente[i] > maiorCliente[i]){
-                maiorCliente[i] = nomesCliente[i];
-        }
-        if(marcas[i] > marcaMaiorPreco[i]){
-                marcaMaiorPreco[i] = marcas[i];
+    public void cadastrar(){
+        solicitarInformacao(atual);
+        atual++;
     }
-}
-    public void apresentarVendas(int posicao) {
+    public void decobrirAMaiorVenda(){
+        int maiorVendas = Integer.MIN_VALUE;
+        String nomeFuncionarMaiorVenda = "";
+        int valorVendas = 0;
+        String maiorNomeCliente = "";
+        String nomeMaiorCelular = "0";
+        for(int i = 0; i < atual; i++){
+            if(maiorVendas > numVendas[i]){
+                nomeFuncionarMaiorVenda = nomeFuncionario[i];
+                maiorNomeCliente = nomeCliente[i];
+                nomeMaiorCelular = nomeCelular [i];
+            }
+        }
         JOptionPane.showInputDialog(null,
-                "Funcionario do MÊS: " + maiorVendas
-                + "\nNome Cliente: " + maiorClient
-                + "\nCelular Maior Preço: " + marcaMaiorPreco);
-
+                "Funcionario do MÊS: " + nomeFuncionarMaiorVenda
+                + "\nQuantidade: " + maiorVendas
+                + "\nNome Cliente: " + maiorNomeCliente
+                + "\nCelular Maior Preço: " + nomeMaiorCelular);
     }
 }
